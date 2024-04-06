3fead9a387f8075d18155a5779897f45409bbbcc
Michael Fiedler <michael.fiedler1604@gmail.com>
2024-04-06 12:19:19 +0200

    added main function

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log('hello world')
+const { argv } = require('node:process');
+
+function main(){
+  if (argv.length > 3){
+    throw new Error('Too many arguments given')
+  }
+  if (argv.length === 2){
+    throw new Error('Not enough arguments given')
+  }
+  const baseURL = argv[2]
+  console.log(`Start crawling at ${baseURL}`)
+}
+
+main()
